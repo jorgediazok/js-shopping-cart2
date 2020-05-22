18b920d30eb0805f7ce970edc2cb13dc0b722ed1
Jorge <jorgediazok@gmail.com>
2020-05-22 02:33:27 -0300

    final project

--- js/app.js
+++ js/app.js
@@ -13,13 +13,13 @@
   const cartBtn = document.querySelectorAll('.store-item-icon');
 
   cartBtn.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
+    btn.addEventListener('click', function (event) {
       if (event.target.parentElement.classList.contains('store-item-icon')) {
         let fullPath = event.target.parentElement.previousElementSibling.src;
-        let pos = fullPath.indexOf('img');
+        let pos = fullPath.indexOf('img') + 3;
         let partPath = fullPath.slice(pos);
         const item = {};
-        item.img = `img-carts${partPath}`;
+        item.img = `img-cart${partPath}`;
         let name =
           event.target.parentElement.parentElement.nextElementSibling
             .children[0].children[0].textContent;
@@ -30,7 +30,7 @@
         let finalPrice = price.slice(1).trim();
         item.price = finalPrice;
         const cartItem = document.createElement('div');
-        cartItem.classList(
+        cartItem.classList.add(
           'cart-item',
           'd-flex',
           'justify-content-between',
@@ -51,8 +51,31 @@
             <i class="fas fa-trash"></i>
           </a>
         </div>
-        <!--end of  cart item -->`;
+        <!--end of cart item -->`;
+
+        //select cart
+        const cart = document.getElementById('cart');
+        const total = document.querySelector('.cart-total-container');
+        cart.insertBefore(cartItem, total);
+        alert('item added to the cart');
+        showTotals();
       }
     });
   });
+  //show totals
+  function showTotals() {
+    const total = [];
+    const items = document.querySelectorAll('.cart-item-price');
+    items.forEach(function (item) {
+      total.push(parseFloat(item.textContent));
+    });
+    const totalMoney = total.reduce(function (total, item) {
+      total += item;
+      return total;
+    }, 0);
+    const finalMoney = totalMoney.toFixed(2);
+    document.getElementById('cart-total').textContent = finalMoney;
+    document.querySelector('.item-total').textContent = finalMoney;
+    document.getElementById('item-count').textContent = total.length;
+  }
 })();
